e58a8ecf408b304ca38e3c59104952f78e701874
Martín René <martinvilu@gmail.com>
2024-03-07 10:32:51 -0300

    Archivos Plantilla más como plantilla, sin código de ejemplo.

--- src/main/java/ar/unrn/PlantillaApp.java
+++ src/main/java/ar/unrn/PlantillaApp.java
@@ -4,13 +4,9 @@ package ar.unrn;
  * Esta es una plantilla de main.
  * Usen Shift+F6 sobre el nombre para cambiarlo al que necesiten.
  * ¡Esto lo pueden utilizar en cualquier identificador!
+ * Acá va la consigna del ejercicio.
  */
 public class PlantillaApp {
-
-    /**
-     * Un ejemplo de valor constante.
-     */
-    public static final int MAXIMO = 10;
 
     /**
      * Punto de entrada del ejercicio.
@@ -18,27 +14,6 @@ public class PlantillaApp {
      * @param args son los argumentos de invocación.
      */
     public static void main(String[] args) {
-        // Con Alt+Enter IntelliJ IDEA nos mostrará sugerencias
-        // para modificar el bloque en el que estamos ubicados.
-        System.out.printf("Hola y bienvenido! %s%n", "Estudiante");
-
-        // Con Shift+F10 o la flecha verde del margen,
-        // podemos ejecutar este main
-        for (int i = 0; i <= MAXIMO; i++) {
-            // Con Shift+F9 comenzaremos a debuggear el código.
-            // Y podemos agregar breakpoints con Ctrl+F8.
-            System.out.println("i = " + suma(i, MAXIMO));
-        }
-    }
-
-    /**
-     * Retorna la suma de dos valores.
-     *
-     * @param primer  termino a sumar
-     * @param segundo termino a sumar
-     * @return la suma de ambos terminos
-     */
-    public static int suma(int primer, int segundo) {
-        return primer + segundo;
+        ;// Completar
     }
 }
--- src/test/java/ar/unrn/PlantillaAppTest.java
+++ src/test/java/ar/unrn/PlantillaAppTest.java
@@ -10,24 +10,15 @@ import org.junit.jupiter.api.Test;
  * El nombre tiene que ser el mismo que la clase que testea
  * terminando con `Test` para funcionar.
  */
-@DisplayName("Para 'Plantilla' en src")
+@DisplayName("Ejercicio 1")
 class PlantillaAppTest {
 
     /**
      * Este test es una plantilla.
      */
     @Test
-    @DisplayName("Dos mas cuatro")
-    void sumaPositiva() {
-        int resultado = PlantillaApp.suma(2, 4);
-        int esperado = 6;
-        Assertions.assertEquals(esperado, resultado, "La suma positiva no dio bien");
-    }
-
-    @Test
-    @Disabled // Esto lo desactiva
-    @DisplayName("Este falla a proposito")
-    void falla() {
-        Assertions.fail("Esto tiene sus usos!");
+    @DisplayName("Completar que se esta probando")
+    void testPlantilla() {
+        ;//
     }
 }
